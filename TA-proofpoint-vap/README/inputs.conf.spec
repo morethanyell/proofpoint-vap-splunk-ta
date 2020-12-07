4e7e07dab13fc8dc09b5dd6778915f874f7f8ff5
Daniel Astillero <daniel.l.astillero@gmail.com>
2020-12-07 14:59:52 -0500

    First commit

---
+++ TA-proofpoint-vap/README/inputs.conf.spec
@@ -0,0 +1,3 @@
+[proofpoint_vap://<name>]
+window = An integer indicating how many days the data should be retrieved for. Accepted values are 14, 30 and 90.
+service_principal =
---
+++ TA-proofpoint-vap/bin/TA_proofpoint_vap_rh_proofpoint_vap.py
@@ -0,0 +1,72 @@
+
+import ta_proofpoint_vap_declare
+
+from splunktaucclib.rest_handler.endpoint import (
+    field,
+    validator,
+    RestModel,
+    DataInputModel,
+)
+from splunktaucclib.rest_handler import admin_external, util
+from splunk_aoblib.rest_migration import ConfigMigrationHandler
+
+util.remove_http_proxy_env_vars()
+
+
+fields = [
+    field.RestField(
+        'interval',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.Pattern(
+            regex=r"""^\-[1-9]\d*$|^\d*$""", 
+        )
+    ), 
+    field.RestField(
+        'index',
+        required=True,
+        encrypted=False,
+        default='default',
+        validator=validator.String(
+            min_len=1, 
+            max_len=80, 
+        )
+    ), 
+    field.RestField(
+        'window',
+        required=False,
+        encrypted=False,
+        default='30',
+        validator=None
+    ), 
+    field.RestField(
+        'service_principal',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+
+    field.RestField(
+        'disabled',
+        required=False,
+        validator=None
+    )
+
+]
+model = RestModel(fields, name=None)
+
+
+
+endpoint = DataInputModel(
+    'proofpoint_vap',
+    model,
+)
+
+
+if __name__ == '__main__':
+    admin_external.handle(
+        endpoint,
+        handler=ConfigMigrationHandler,
+    )
